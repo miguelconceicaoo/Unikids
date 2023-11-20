cc0ef52a1295039939dd941c80e5a50bd348bd5e
miguelconceicaoo <miguelconceicao125@gmail.com>
2023-11-19 21:10:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function devs(){
+    var mostrar = document.getElementById('devs').style.display;
+
+    if(mostrar == "none"){
+        document.getElementById('devs').style.display = 'grid';
+        
+    }else{
+        document.getElementById('devs').style.display = 'none';
+    }
+}
+
